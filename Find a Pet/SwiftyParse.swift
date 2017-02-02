f2035fb1edb40647d08e95d28480e58930300567
FlyingDad <Michael@michaels-imac.attlocal.net>
2017-02-02 11:33:38 -0600

    - All core data operations now performed in main queue

--- Find a Pet/SwiftyParse.swift
+++ Find a Pet/SwiftyParse.swift
@@ -27,6 +27,8 @@ class SwiftyParse {
     func parseAndSavePet(json: JSON, zipCode: String, coreDataStack: CoreDataStack) -> Void {
         
         // Save to core data
+        DispatchQueue.main.async {
+
         let petEntity = NSEntityDescription.entity(forEntityName: "Pet", in: coreDataStack.managedContext)!
         let pet = Pet(entity: petEntity, insertInto: coreDataStack.managedContext)
         
@@ -42,38 +44,8 @@ class SwiftyParse {
         pet.shelterId = json[PetFinderConstants.ResponseKeys.Pet.ShelterId][PetFinderConstants.ResponseKeys.General.MysteryT].stringValue
         pet.shelterPetId = json[PetFinderConstants.ResponseKeys.Pet.ShelterPetId][PetFinderConstants.ResponseKeys.General.MysteryT].stringValue
         pet.size = json[PetFinderConstants.ResponseKeys.Pet.Size][PetFinderConstants.ResponseKeys.General.MysteryT].stringValue
-//        print(pet.name!)
-//        print(pet.age!)
-//        print(pet.animal!)
-//        print(pet.desc!)
-//        print(pet.id!)
-//        print(pet.lastUpdate!)
-//        print(pet.mix!)
-//        print(pet.sex!)
-//        print(pet.shelterId!)
-//        print(pet.shelterPetId!)
-//        print(pet.size!)
-//        print("Saving pet \(pet.name)")
-        //parsePhotos(json: json, managedContext: managedContext, pet: pet)
-        //parseOptions(json: json, managedContext: managedContext, pet: pet)
-        //parseBreeds(json: json, managedContext: managedContext, pet: pet)
-        
-        //Save pet
-//        DispatchQueue.main.async {
-//            //print("saving pet \(pet.name)")
-//            coreDataStack.saveContext()
-//        }
-        parsePhotos(json: json, coreDataStack: coreDataStack, pet: pet)
-        parseOptions(json: json, coreDataStack: coreDataStack, pet: pet)
-        parseBreeds(json: json, coreDataStack: coreDataStack, pet: pet)
-       //return pet
-    }
 
-    
-    // Parse and save pet photos
-    func parsePhotos(json: JSON, coreDataStack: CoreDataStack, pet: Pet) {
-        
-        
+
         let photoArray = json[PetFinderConstants.ResponseKeys.Pet.Media][PetFinderConstants.ResponseKeys.Pet.Photos][PetFinderConstants.ResponseKeys.Photo.Photo]
         //print(photoArray)
         for (index, photoData):(String, JSON) in photoArray {
@@ -89,38 +61,15 @@ class SwiftyParse {
 
                 pet.addToPhotos(photo)
                 //print("saving pet photo for \(pet.name) index \(index)")
-                DispatchQueue.main.async {
-                    //print("saving pet photo for \(pet.name) index \(index)")
-                    //print(photo.url)
-                    coreDataStack.saveContext()
-                }
+//                DispatchQueue.main.async {
+//                    //print("saving pet photo for \(pet.name) index \(index)")
+//                    //print(photo.url)
+//                    coreDataStack.saveContext()
+//                }
                 
             }
         }
-        
 
-//                    // save photo
-//                    //                    DispatchQueue.main.async {
-//                    //                        print("saving pet photos")
-//                    //                        do {
-//                    //                            try self.managedContext.save()
-//                    //                        } catch let error as NSError {
-//                    //                            print("Photo save error: \(error)")
-//                    //                        }
-//                    //                    }
-//                }
-//            }
-//        }
-    }
-
-    // Parse and save pet options
-    func parseOptions(json: JSON, coreDataStack: CoreDataStack, pet: Pet) {
-        
-        //let optionsEntity = NSEntityDescription.entity(forEntityName: "Options", in: coreDataStack.managedContext)!
-        //let options = Options(entity: optionsEntity, insertInto: coreDataStack.managedContext)
-        
-        
-        //print("Processing options")
         if let optionArray = json[PetFinderConstants.ResponseKeys.Pet.Options][PetFinderConstants.ResponseKeys.Option.Option].array {
             for option in optionArray {
                 let options = NSEntityDescription.insertNewObject(forEntityName: "Options", into: coreDataStack.managedContext) as! Options
@@ -129,46 +78,26 @@ class SwiftyParse {
                 //print(options.option!)
                 pet.addToOptions(options)
             }
-            DispatchQueue.main.async {
-                //print("saving pet photo for \(pet.name) index \(index)")
-                //print(photo.url)
-                coreDataStack.saveContext()
-            }
+//            DispatchQueue.main.async {
+//                //print("saving pet photo for \(pet.name) index \(index)")
+//                //print(photo.url)
+//                coreDataStack.saveContext()
+//            }
         } else if let optionsDict = json[PetFinderConstants.ResponseKeys.Pet.Options][PetFinderConstants.ResponseKeys.Option.Option].dictionary {
             let options = NSEntityDescription.insertNewObject(forEntityName: "Options", into: coreDataStack.managedContext) as! Options
             options.option = optionsDict[PetFinderConstants.ResponseKeys.General.MysteryT]?.stringValue
             //options.pet = pet
             //print(options.option!)
             pet.addToOptions(options)
-            DispatchQueue.main.async {
-                //print("saving pet photo for \(pet.name) index \(index)")
-                //print(photo.url)
-                coreDataStack.saveContext()
-            }
+//            DispatchQueue.main.async {
+//                //print("saving pet photo for \(pet.name) index \(index)")
+//                //print(photo.url)
+//                coreDataStack.saveContext()
+//            }
 
         }
         
 
-        //                    // save option
-        //                    //                    DispatchQueue.main.async {
-        //                    //                        print("saving pet photos")
-        //                    //                        do {
-        //                    //                            try self.managedContext.save()
-        //                    //                        } catch let error as NSError {
-        //                    //                            print("Photo save error: \(error)")
-        //                    //                        }
-        //                    //                    }
-        //                }
-        //            }
-        //        }
-    }
-
-    // Parse and save pet breeds
-    func parseBreeds(json: JSON, coreDataStack: CoreDataStack, pet: Pet) {
-        
-        //let breedsEntity = NSEntityDescription.entity(forEntityName: "Breeds", in: coreDataStack.managedContext)!
-        //let breeds = Breeds(entity: breedsEntity, insertInto: coreDataStack.managedContext)
-        //print("Processing breeds")
         
         // SwiftyJSON returns array if array od dict, or dict if only ONE dict in array
         if let breedsArray = json[PetFinderConstants.ResponseKeys.Pet.Breeds][PetFinderConstants.ResponseKeys.Breeds.Breed].array {
@@ -177,11 +106,11 @@ class SwiftyParse {
                 breeds.breed = breed[PetFinderConstants.ResponseKeys.General.MysteryT].stringValue
                 breeds.pet = pet
                 //print(breeds.breed!)
-                DispatchQueue.main.async {
-                    //print("saving pet photo for \(pet.name) index \(index)")
-                    //print(photo.url)
-                    coreDataStack.saveContext()
-                }
+//                DispatchQueue.main.async {
+//                    //print("saving pet photo for \(pet.name) index \(index)")
+//                    //print(photo.url)
+//                    coreDataStack.saveContext()
+//                }
 
             }
         } else
@@ -189,43 +118,15 @@ class SwiftyParse {
                 let breeds = NSEntityDescription.insertNewObject(forEntityName: "Breeds", into: coreDataStack.managedContext) as! Breeds
             breeds.breed = breedsDict[PetFinderConstants.ResponseKeys.General.MysteryT]?.stringValue
             breeds.pet = pet
-                DispatchQueue.main.async {
-                    //print("saving pet photo for \(pet.name) index \(index)")
-                    //print(photo.url)
-                    coreDataStack.saveContext()
-                }
+//                DispatchQueue.main.async {
+//                    //print("saving pet photo for \(pet.name) index \(index)")
+//                    //print(photo.url)
+//                    coreDataStack.saveContext()
+//                }
 
             //print(breeds.breed!)
         }
-        
-        
-        
-//        if let breedsArray = json[PetFinderConstants.ResponseKeys.Pet.Breeds][PetFinderConstants.ResponseKeys.Breeds.Breed].array
-//        {
-//        print("Breeds count: \(breedsArray.count)")
-//        print("Breeds Array: \(breedsArray)")
-//        if breedsArray.count > 0 {
-//            for (index, breedData):(String, JSON) in breedsArray {
-//                breed.breed = breedData[PetFinderConstants.ResponseKeys.General.MysteryT].string
-//                print("Breed: \(index): \(breed.breed)")
-//                breed.pet = pet
-//            }
-//        }
-        
-        
-        //                    // save option
-        //                    //                    DispatchQueue.main.async {
-        //                    //                        print("saving pet photos")
-        //                    //                        do {
-        //                    //                            try self.managedContext.save()
-        //                    //                        } catch let error as NSError {
-        //                    //                            print("Photo save error: \(error)")
-        //                    //                        }
-        //                    //                    }
-        //                }
-        //            }
-        //        }
-    
+        coreDataStack.saveContext()
     }
-    
+    }
 }
