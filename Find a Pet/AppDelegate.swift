36d1df6639ebcdbaf4a73ffcc04075dceb42e432
FlyingDad <Michael@michaels-imac.attlocal.net>
2017-02-03 15:11:39 -0600

    Added last animal type searched to user defaults

--- Find a Pet/AppDelegate.swift
+++ Find a Pet/AppDelegate.swift
@@ -33,7 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func checkIfFirstLaunch() {
         if !(UserDefaults.standard.bool(forKey: "hasLaunchedBefore")) {
             UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
-            UserDefaults.standard.set("00000", forKey: "zipCodeLastSearched")
+            UserDefaults.standard.set("89121", forKey: "zipCodeLastSearched")
+            UserDefaults.standard.set("bird", forKey: "animalTypeLastSearched")
             UserDefaults.standard.synchronize()
         }
     }
@@ -47,6 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         coreDataStack.saveContext()
+        UserDefaults.standard.synchronize()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -61,6 +63,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         coreDataStack.saveContext()
+        UserDefaults.standard.synchronize()
     }
 
 }
