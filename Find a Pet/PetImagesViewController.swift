0adf026db2a70538624a5053072cc5b915fd09cb
Michael <mkroth65@gmail.com>
2017-09-13 09:35:45 -0500

    Adjusted flow layout on collection view and added double tap gesture recognizor on zoom view.

--- Find a Pet/PetImagesViewController.swift
+++ Find a Pet/PetImagesViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 import CoreData
 
-class PetImagesViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class PetImagesViewController: UICollectionViewController {
+    
+    let sectionInsets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 20.0, right: 10.0)
+    let cellsPerRow: CGFloat = 3
     
     var coreDataStack: CoreDataStack!
     var pet: Pet!
@@ -25,8 +28,6 @@ class PetImagesViewController: UICollectionViewController, UICollectionViewDeleg
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         }
-
-
     }
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -75,20 +76,20 @@ class PetImagesViewController: UICollectionViewController, UICollectionViewDeleg
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsetsMake(10, 10, 10, 10)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        //print(collectionView.frame.width)
-        // set two images per row in collection view portrait mode
-        // 10 inset * 2 and 10 spacing = 30
-        let imageWidth = (collectionView.frame.width - 30)/2
-        //let sideSize = (traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular) ? 190.0 : 128.0
-        return CGSize(width: imageWidth, height: imageWidth)
-    }
-    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsetsMake(10, 10, 10, 10)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        
+//        //print(collectionView.frame.width)
+//        // set two images per row in collection view portrait mode
+//        // 10 inset * 2 and 10 spacing = 30
+//        let imageWidth = (collectionView.frame.width - 30)/2
+//        //let sideSize = (traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular) ? 190.0 : 128.0
+//        return CGSize(width: imageWidth, height: imageWidth)
+//    }
+//    
 
     func getPetImagesArray () {
         if let photosArray = pet.photos?.allObjects as? [Photos] {
@@ -115,5 +116,33 @@ class PetImagesViewController: UICollectionViewController, UICollectionViewDeleg
         }
     }
     
+    
+}
+
+// Code from https://www.raywenderlich.com/136159/uicollectionview-tutorial-getting-started
+
+extension PetImagesViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // Calculate the cell width based on padding and screen size
+        let paddingSpace = sectionInsets.left * (cellsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace - 1
+        
+        let widthPerCell = availableWidth / cellsPerRow
+        
+        // return square cell size
+        return CGSize(width: widthPerCell, height: widthPerCell)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        // return same spacing as padding inset
+        return sectionInsets.left
+    }
     
 }
--- Find a Pet/ZoomPhotoViewController.swift
+++ Find a Pet/ZoomPhotoViewController.swift
@@ -22,18 +22,35 @@ class ZoomPhotoViewController: UIViewController, UIScrollViewDelegate {
         
         super.viewDidLoad()
         
-        self.scrollView.maximumZoomScale = 1.0
-        self.scrollView.maximumZoomScale = 6.0
+        self.scrollView.minimumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 4.0
         
         if let image = UIImage(data: photo.imageData! as Data) {
             imageView.image = image
         } else {
             //print("not getting image")
         }
+        
+        setupGestureRecognizer()
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
+    }
+    
+    func setupGestureRecognizer() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap))
+        doubleTap.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(doubleTap)
+    }
+    
+    func handleDoubleTap() {
+        
+        if (scrollView.zoomScale > scrollView.minimumZoomScale) {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        } else {
+            scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
+        }
     }
 
 }
